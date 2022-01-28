492205f7949778b42a8e62bf2e33d946e6ef59af
mihail.gaberov <mihail.gaberov@ysura.com>
2022-01-28 17:12:28 +0100

    :sparkles: Add Loader, PageTransition and PageHeading

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,59 +1,42 @@
-<!-- This is the global layout file; it "wraps" every page on the site. (Or more accurately: is the parent component to every page component on the site.) -->
-<script context="module">
-	export const load = async({ url, fetch }) => {
-    /**
-     * This fetch call is not used in this file, but the route won't be pre-rendered 
-     * and routed properly unless it's called inside a `load` function. ¯\_(ツ)_/¯
-     * */ 
-    const rss = await fetch(`/api/rss.xml`)
+<script context="module" lang="ts">
+	import type { LoadOutput } from '@sveltejs/kit'
 
-    return {
-      props: {
-        path: url.pathname
-      }
-    }
-  }
+	export const load = async ({ url }): Promise<LoadOutput> => {
+		const path: string = url.pathname
+
+		return {
+			props: {
+				path
+			}
+		}
+	}
 </script>
 
-<script>
+<script lang="ts">
   import '$lib/assets/scss/global.scss'
   import Header from '$lib/components/Header.svelte'
   import Footer from '$lib/components/Footer.svelte'
-  import { currentPage, isMenuOpen } from '$lib/data/store'
+  import { currentPage, isMenuOpen, isLoading } from '$lib/data/store'
   import { navItems } from '$lib/config'
 	import { prefetch } from '$app/navigation'
   import { onMount } from 'svelte'
-  import { fade } from 'svelte/transition'
-  import Callout from '$lib/components/Callout.svelte';
-
-  const transitionIn = { delay: 150, duration: 150 }
-  const transitionOut = { duration: 100 }
+  import Loader from '$lib/components/Loader.svelte'
+  import PageTransition from '$lib/components/PageTransition.svelte';
+  import PageHeading from '$lib/components/PageHeading.svelte';
 
   export let path
   
-  /**
-   * Updates the global store with the current path. (Used for highlighting 
-   * the current page in the nav, but could be useful for other purposes.)
-   **/
+  const setLoading = (newState: boolean): void => {
+		isLoading.set(newState)
+	}
+
   $: currentPage.set(path)
 
-  /**
-   * This pre-fetches all top-level routes on the site in the background for faster loading.
-   * https://kit.svelte.dev/docs#modules-$app-navigation
-   * 
-   * Any route added in src/lib/config.js will be prefetched automatically. You can add your
-   * own prefetch() calls here, too.
-   **/
   onMount(() => {
     navItems.forEach(item => prefetch(item.route))
   })
 </script>
-
-
 <!-- 
-  The below markup is used on every page in the site. The <slot> is where the page's
-  actual contents will show up.
--->
 <div class="layout" class:open={$isMenuOpen}>
   <Header />
   {#key path}
@@ -67,4 +50,20 @@
     </main>
   {/key}
   <Footer />
+</div> -->
+
+<div id="app">
+	<Loader loading={$isLoading}/>
+
+	<Header /> 
+
+	<div class="layout"> 
+		<PageHeading title={path} />
+
+		<PageTransition refresh={path}>
+			<slot/>
+		</PageTransition>
+	</div>
+
+	<Footer />
 </div>
