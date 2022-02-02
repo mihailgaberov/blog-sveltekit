1182170fb9da8a72cd5da3b2a79caeb7f2fcaca1
mihail.gaberov <mihail.gaberov@ysura.com>
2022-02-02 09:51:19 +0100

    :sparkles: Read site title from config

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,10 @@
+<script>
+import { siteTitle } from '$lib/config'
+</script> -->
+
+
 <svelte:head>
-	<title>Personal blog by Mihail Gaberov.</title>
+	<title>{siteTitle}</title>
 </svelte:head>
 
 <h1><img class="head-photo" src="https://mihail-gaberov.eu/static/profile-pic-538ec3f11211b7561f61b99ab54fb65f.jpg" alt="Mihail Gaberov" title="Mihail Gaberov" />Hi, I'm Mihail.</h1>
